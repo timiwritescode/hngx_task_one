b972257ea568118c3dff901730d065fcbdb84306
Oluwatimilehin <odebodezion@gmail.com>
2023-09-07 07:38:11 +0100

    fix production issues

--- api/app.py
+++ api/app.py
@@ -1,8 +1,8 @@
 from engine import api
 from flask_restful import Resource
 from flask import jsonify
-from engine.arguments import main_get_arguments
-from engine.util.utc_time import get_current_utc_time, get_current_day
+from arguments import main_get_arguments
+from util.utc_time import get_current_utc_time, get_current_day
 from flask import Flask
 
 app = Flask(__name__)
