46e06b33112fb399ca295c5702bba5b8986593de
Oluwatimilehin <odebodezion@gmail.com>
2023-09-07 06:47:57 +0100

    Add config

--- api/app.py
+++ api/app.py
@@ -1,5 +1,15 @@
-from engine import app
+#from engine import app
 
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api')
+def hello():
+    return "Hello Vercel"
+
