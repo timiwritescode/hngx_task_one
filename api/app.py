b0dd174753174781e2622e41cb966e4bdce9773a
Oluwatimilehin <odebodezion@gmail.com>
2023-09-07 07:02:41 +0100

    fix production issues

--- api/app.py
+++ api/app.py
@@ -1,8 +1,8 @@
-from engine import api
-from flask_restful import Resource
-from flask import jsonify
-from engine.arguments import main_get_arguments
-from engine.util.utc_time import get_current_utc_time, get_current_day
+#from engine import api
+#from flask_restful import Resource
+#from flask import jsonify
+#from engine.arguments import main_get_arguments
+#from engine.util.utc_time import get_current_utc_time, get_current_day
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,6 +10,8 @@ app = Flask(__name__)
 @app.route('/api')
 def hello():
     return "Hello Vercel"
+
+
 # each key of the json data are entered into 
 
 """
